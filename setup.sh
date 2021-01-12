e3c62b6ad032e2b46a8961533cc46ff58cb5fe0f
Santosh A <santosh7apr@gmail.com>
2021-01-12 21:25:26 +0530

    setup.sh updated

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 mkdir -p ~/.streamlit/
 
-echo “\
+echo "\
 [server]\n\
-port = $PORT\n\
-enableCORS=false\n\
 headless = true\n\
-\n\
-“ > ~/.streamlit/config.toml
+enableCORS=false\n\
+port = $PORT\n\
+">~/.streamlit/config.toml
